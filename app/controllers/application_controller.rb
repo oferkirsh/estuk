e2149fca26cfa3266d7e1f073052bdbed700e818
ofer <ofer@ofer-VirtualBox.(none)>
2015-02-16 16:03:06 +0200

    add flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
-  	redirect_to root_path notice: "You are not authrized to access this page"
+  	redirect_to root_path, notice: "You are not authrized to access this page"
   end
 end
