211507a0bba90f2edcfde3ba8e9bd4ba0929bced
ofer <ofer@ofer-VirtualBox.(none)>
2015-02-16 23:59:52 +0200

    add sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,10 @@ class Book < ActiveRecord::Base
 	friendly_id :name, use: :slugged
 
 	belongs_to :user
+	has_many :sales
 	has_attached_file :image
 	has_attached_file :resource
+
 
 	validates_attachment_content_type :image, 
 	content_type:  /^image\/(png|gif|jpeg)/,
@@ -16,5 +18,7 @@ class Book < ActiveRecord::Base
 	 
 	validates :image, attachment_presence: true
 	validates :resource, attachment_presence: true
+	validates_numericality_of :price,
+	greater_than: 49, message: "must be at least 50 cents"
 
 end
